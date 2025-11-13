0e934e7ca3721f7d2826306b25e0936b38e3acf2
hb <hb.yu.asic@outlook.com>
2025-11-13 19:14:58 +0800

    update keymap for split and nvtree toggle

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<C-w>|", "<CMD>vsplit<CR>", {  desc = "Split vertically" })
+map("n", "<C-w>_", "<CMD>split<CR>", {  desc = "Split horizontally" })
+
+map("n", "<C-w>e", ":NvimTreeToggle<CR>", {  desc = "Toggle NvimTree" })
